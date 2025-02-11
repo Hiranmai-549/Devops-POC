cc1e4b70c41e8a47b9619a325453bf690cca1759
Hiranmai-549 <hiranmai.aws1998@gmail.com>
2025-02-11 15:02:18 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["a", "b", "c"]
+}
