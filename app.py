fdf1d75efe4d4a9bd58771f06aa04c3167d3b0ed
Hiranmai-549 <hiranmai.aws1998@gmail.com>
2025-02-11 21:09:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-# Dockerfile
-FROM python:3.7
-WORKDIR /app
-COPY app.py /app
-RUN pip install flask
-CMD ["python", "app.py"]
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def gethostname():
+    return f"Container ID is: {socket.gethostname()} This is the dev branch."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
